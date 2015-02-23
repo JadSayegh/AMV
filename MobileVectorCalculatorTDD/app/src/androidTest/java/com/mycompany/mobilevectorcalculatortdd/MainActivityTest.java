4f0941096700823dbb5c7c5a488e33df0210bd97
JadSayegh <jad.r.sayegh@gmail.com>
2015-02-23 10:23:08 -0500

    completed test case

--- MobileVectorCalculatorTDD/app/src/androidTest/java/com/mycompany/mobilevectorcalculatortdd/MainActivityTest.java
+++ MobileVectorCalculatorTDD/app/src/androidTest/java/com/mycompany/mobilevectorcalculatortdd/MainActivityTest.java
@@ -5,6 +5,7 @@ import android.test.UiThreadTest;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,6 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 /**
  * Created by Jad on 2015-02-21.
@@ -19,13 +21,14 @@ import android.widget.RadioGroup;
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     MainActivity activity;
+    SecondActivity secondActivity;
     final double eps = 0.001; //maximum tolerable error in a double to allow for potential rounding difference
+    final int NUMERICAL_DECIMAL_SIGNED_INPUT = InputType.TYPE_NUMBER_FLAG_SIGNED | InputType.TYPE_NUMBER_FLAG_DECIMAL + 2;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         MainActivityTest testcase = new MainActivityTest();
 
     }
-
 
 
     public MainActivityTest() {
@@ -50,106 +53,121 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 
     //==================== VALID INPUT TESTS ===========================
 
-    public void testVectorAddCart() {
+    public void testVectorAdd() {
         Vector expected = new Vector(5, 8);
-        Vector actual = activity.vectorAdd(new Vector(3, 13), new Vector(2, -5));
+        Vector actual = cartesianCoordinate.vectorAdd(new Vector(3, 13), new Vector(2, -5));
         assertEquals(actual.x, expected.x);
         assertEquals(actual.y, expected.y);
 
         expected = new Vector(7, 6);
-        actual = activity.vectorAdd(new Vector(3, 13), new Vector(2, -5), new Vector(2, -2));
+        actual = cartesianCoordinate.vectorAdd(new Vector(3, 13), new Vector(2, -5), new Vector(2, -2));
         assertEquals(actual.x, expected.x);
         assertEquals(actual.y, expected.y);
 
 
+    }
 
-    }
-    public void testVectorAddOrder() {
-        Vector expected = new Vector(5, 8);
-        Vector actual = activity.vectorAdd(new Vector(2, -5), new Vector(3, 13));
-        assertEquals(actual.x, expected.x);
-        assertEquals(actual.y, expected.y);
-    }
 
 
     public void testVectorMult() {
         double expected = 8;
-        double actual = activity.vectorMult(new Vector(2, 3), new Vector(-2, 1));
+        double actual = cartesianCoordinate.vectorMult(new Vector(2, 3), new Vector(-2, 1));
         assertEquals(actual, expected);
     }
 
     public void testVectorMultOrder() {
-        double expected = - 8;
-        double actual = activity.vectorMult(new Vector(-2, 1), new Vector(2, 3));
-        assertEquals(actual, expected);
+
+        double first = cartesianCoordinate.vectorMult(new Vector(-2, 1), new Vector(2, 3));
+        double second = cartesianCoordinate.vectorMult(new Vector(2, 3), new Vector(-2, 1));
+
+        assertEquals(first + second, 0.0);
 
     }
 
     public void testScalarMult() {
         double expected = -1;
-        double actual = activity.scalarMult(new Vector(-2, 1), new Vector(2, 3));
+        double actual = cartesianCoordinate.scalarMult(new Vector(-2, 1), new Vector(2, 3));
         assertEquals(actual, expected);
 
     }
 
-    public void testScalarMultOrder() {
-        double expected = -1;
-        double actual = activity.scalarMult(new Vector(2, 3), new Vector(-2, 1));
-        assertEquals(actual, expected);
-    }
 
 
-    public void testCartToPolar(){
-        PolarVector expected = new PolarVector(5, Math.atan2(4, 3));
-        PolarVector actual = activity.cartToPolar(new Vector(3,4));
+    public void testCartToPolar() {
+        PolarVector expected = new PolarVector(5, Math.toDegrees(Math.atan2(4, 3)));
+        PolarVector actual = PolarCoordinate.cartesianToPolar(new Vector(3, 4));
         assertTrue(Math.abs(actual.theta - expected.theta) < eps);
-        assertTrue(Math.abs(actual.r - expected.r) < eps);
+        assertTrue(Math.abs(actual.rval - expected.rval) < eps);
     }
 
-    public void testPolarToCart(){
-        Vector expected = new Vector(3,4);
-        Vector actual = activity.polarToCart(new PolarVector(5, Math.atan2(4, 3)));
+    public void testPolarToCart() {
+        Vector expected = new Vector(3, 4);
+        Vector actual = PolarCoordinate.polarToCartesian(new PolarVector(5, Math.toDegrees(Math.atan2(4, 3))));
         assertTrue(Math.abs(actual.x - expected.x) < eps);
         assertTrue(Math.abs(actual.y - expected.y) < eps);
     }
 
     //==================== INVALID INPUT TESTS ===========================
 
-    public void testVectorMult_NullInput(){
+    public void testVectorMult_NullInput() {
         Double expected = null;
-        Double actual = activity.vectorMult(new Vector(2, 3), null);
+        Double actual = cartesianCoordinate.vectorMult(new Vector(2, 3), null);
         assertEquals(actual, expected);
     }
 
-    public void testScalarMult_NullInput(){
+    public void testScalarMult_NullInput() {
         Double expected = null;
-        Double actual = activity.scalarMult(new Vector(2, 3), null);
+        Double actual = cartesianCoordinate.scalarMult(new Vector(2, 3), null);
         assertEquals(actual, expected);
     }
 
-    public void testVectorAdd_NullInput(){
+    public void testVectorAdd_NullInput() {
         Vector expected = null;
-        Vector actual = activity.vectorAdd(null, new Vector(2, 3));
+        Vector actual = cartesianCoordinate.vectorAdd(null, new Vector(2, 3));
         assertEquals(actual, expected);
     }
 
 
-    public void testCartToPolar_NullInput(){
+
+
+    public void testCartToPolar_NullInput() {
         PolarVector expected = null;
-        PolarVector actual = activity.cartToPolar(null);
+        PolarVector actual = PolarCoordinate.cartesianToPolar(null);
         assertEquals(actual, expected);
     }
 
-    public void testPolarToCart_NullInput(){
+
+    public void testPolarToCart_NullInput() {
         Vector expected = null;
-        Vector actual = activity.polarToCart(null);
+        Vector actual = PolarCoordinate.polarToCartesian(null);
         assertEquals(actual, expected);
     }
 
 
-    //======================= UI Tests ==================
+//======================= UI Tests ==================
+    public void testKeyboardOnlyNumerical() {
+
+
+        EditText editTextx1 = (EditText) activity.findViewById(R.id.x1);
+        EditText editTextx2 = (EditText) activity.findViewById(R.id.x2);
+        EditText editTextx3 = (EditText) activity.findViewById(R.id.x3);
+
+        EditText editTexty1 = (EditText) activity.findViewById(R.id.y1);
+        EditText editTexty2 = (EditText) activity.findViewById(R.id.y2);
+        EditText editTexty3 = (EditText) activity.findViewById(R.id.y3);
+
+        assertTrue(editTextx1.getInputType() == NUMERICAL_DECIMAL_SIGNED_INPUT);
+        assertTrue(editTextx2.getInputType() == NUMERICAL_DECIMAL_SIGNED_INPUT);
+        assertTrue(editTextx3.getInputType() == NUMERICAL_DECIMAL_SIGNED_INPUT);
+        assertTrue(editTexty1.getInputType() == NUMERICAL_DECIMAL_SIGNED_INPUT);
+        assertTrue(editTexty2.getInputType() == NUMERICAL_DECIMAL_SIGNED_INPUT);
+        assertTrue(editTexty3.getInputType() == NUMERICAL_DECIMAL_SIGNED_INPUT);
+
+    }
+
+
     @UiThreadTest
-    public void testIncompleteInput(){
+    public void testNullInputErrorFlag(){
         Button calculate = (Button) activity.findViewById(R.id.button1);
         EditText editTextx1 = (EditText) activity.findViewById(R.id.x1);
         EditText editTextx2 = (EditText) activity.findViewById(R.id.x2);
@@ -165,49 +183,18 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         editTexty2.setText("");
 
         RadioGroup radgroup = (RadioGroup)  activity.findViewById(R.id.cartesian_radio);
-        
-    }
+        RadioButton radiobutton = (RadioButton) activity.findViewById(R.id.vector_addition);
+        radiobutton.setChecked(true);
 
+        //TextView textOutput = (TextView) secondActivity.findViewById(R.id.cartesianCoordinate_vectorRes);
+        calculate.performClick();
 
-    @UiThreadTest
-    public void testVectorAddOutputDisplay(){
-        EditText editTextx1 = (EditText) activity.findViewById(R.id.x1);
-        EditText editTextx2 = (EditText) activity.findViewById(R.id.x2);
-        EditText editTextx3 = (EditText) activity.findViewById(R.id.x3);
-
-        EditText editTexty1 = (EditText) activity.findViewById(R.id.y1);
-        EditText editTexty2 = (EditText) activity.findViewById(R.id.y2);
-        EditText editTexty3 = (EditText) activity.findViewById(R.id.y3);
-
-
-
-
+        String s = editTexty2.getError().toString();
+        assertTrue(editTexty2.getError().toString().equals("Invalid input")) ;
 
     }
 
-    public void testVectorMultOutputDisplay(){
-        EditText editTextx1 = (EditText) activity.findViewById(R.id.x1);
-        EditText editTextx2 = (EditText) activity.findViewById(R.id.x2);
-        EditText editTextx3 = (EditText) activity.findViewById(R.id.x3);
 
-        EditText editTexty1 = (EditText) activity.findViewById(R.id.y1);
-        EditText editTexty2 = (EditText) activity.findViewById(R.id.y2);
-        EditText editTexty3 = (EditText) activity.findViewById(R.id.y3);
-
-
-    }
-
-    public void testScalarMultOutputDisplay(){
-        EditText editTextx1 = (EditText) activity.findViewById(R.id.x1);
-        EditText editTextx2 = (EditText) activity.findViewById(R.id.x2);
-        EditText editTextx3 = (EditText) activity.findViewById(R.id.x3);
-
-        EditText editTexty1 = (EditText) activity.findViewById(R.id.y1);
-        EditText editTexty2 = (EditText) activity.findViewById(R.id.y2);
-        EditText editTexty3 = (EditText) activity.findViewById(R.id.y3);
-
-
-    }
 
 }
 
